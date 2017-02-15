e4106347e0b0df5e603ae02e1bd9b2aa082f3380
Matthew <tianlang2060@163.com>
2017-02-15 22:20:33 +0800

    delete: cut out the useless codes - USB, bootp, filesystem;

--- include/configs/smdk2440.h
+++ include/configs/smdk2440.h
@@ -11,18 +11,20 @@
  * High Level Configuration Options
  * (easy to change)
  */
-#define CONFIG_S3C24X0		/* This is a SAMSUNG S3C24x0-type SoC */
-#define CONFIG_S3C2440		/* specifically a SAMSUNG S3C2440 SoC */
-#define CONFIG_SMDK2440		/* on a SAMSUNG SMDK2440 Board */
+#define CONFIG_S3C24X0						/* This is a SAMSUNG S3C24x0-type SoC */
+#define CONFIG_S3C2440						/* specifically a SAMSUNG S3C2440 SoC */
+#define CONFIG_SMDK2440						/* on a SAMSUNG SMDK2440 Board */
+
+#define CONFIG_USER_CUT						/* to cut out the useless codes */
 
 #define CONFIG_SYS_TEXT_BASE	0x0
 
 #define CONFIG_SYS_ARM_CACHE_WRITETHROUGH
 
-/* input clock of PLL (the SMDK2410 has 12MHz input clock) */
-#define CONFIG_SYS_CLK_FREQ	12000000
+/* input clock of PLL (the SMDK2440 has 12MHz input clock) */
+#define CONFIG_SYS_CLK_FREQ		12000000
 
-#define CONFIG_CMDLINE_TAG	/* enable passing of ATAGs */
+#define CONFIG_CMDLINE_TAG					/* enable passing of ATAGs */
 #define CONFIG_SETUP_MEMORY_TAGS
 #define CONFIG_INITRD_TAG
 
@@ -31,9 +33,9 @@
  */
 /* enable DM9000, disable CS8900 */
 #if 0
-#define CONFIG_CS8900		/* we have a CS8900 on-board */
-#define CONFIG_CS8900_BASE	0x19000300
-#define CONFIG_CS8900_BUS16	/* the Linux driver does accesses as shorts */
+#define CONFIG_CS8900						/* we have a CS8900 on-board */
+#define CONFIG_CS8900_BASE		0x19000300
+#define CONFIG_CS8900_BUS16					/* the Linux driver does accesses as shorts */
 
 #else
 /*
@@ -52,29 +54,34 @@
  * select serial console configuration
  */
 #define CONFIG_S3C24X0_SERIAL
-#define CONFIG_SERIAL1		1	/* we use SERIAL 1 on SMDK2440 */
+#define CONFIG_SERIAL1			1			/* we use SERIAL 1 on SMDK2440 */
 
 /************************************************************
  * USB support (currently only works with D-cache off)
  ************************************************************/
+/* Some Macros are defined in configs/smdk2440_defconfig */
+#ifndef CONFIG_USER_CUT
 #define CONFIG_USB_OHCI
 #define CONFIG_USB_OHCI_S3C24XX
 #define CONFIG_DOS_PARTITION
+#endif
 
 /************************************************************
  * RTC
  ************************************************************/
 #define CONFIG_RTC_S3C24X0
 
-#define CONFIG_BAUDRATE		115200
+#define CONFIG_BAUDRATE			115200
 
 /*
  * BOOTP options
  */
+#ifndef CONFIG_USER_CUT
 #define CONFIG_BOOTP_BOOTFILESIZE
 #define CONFIG_BOOTP_BOOTPATH
 #define CONFIG_BOOTP_GATEWAY
 #define CONFIG_BOOTP_HOSTNAME
+#endif
 
 /*
  * Command line configuration.
@@ -87,30 +94,29 @@
 #define CONFIG_CMDLINE_EDITING
 
 /* autoboot */
-#define CONFIG_BOOT_RETRY_TIME	-1
+#define CONFIG_BOOT_RETRY_TIME		-1
 #define CONFIG_RESET_TO_RETRY
 
-#define CONFIG_NETMASK		255.255.255.0
-#define CONFIG_IPADDR		10.0.0.110
-#define CONFIG_SERVERIP		10.0.0.1
+#define CONFIG_NETMASK				255.255.255.0
+#define CONFIG_IPADDR				10.0.0.110
+#define CONFIG_SERVERIP				10.0.0.1
 
 #if defined(CONFIG_CMD_KGDB)
-#define CONFIG_KGDB_BAUDRATE	115200	/* speed to run kgdb serial port */
+#define CONFIG_KGDB_BAUDRATE		115200			/* speed to run kgdb serial port */
 #endif
 
 /*
  * Miscellaneous configurable options
  */
-#define CONFIG_SYS_LONGHELP		/* undef to save memory */
-#define CONFIG_SYS_CBSIZE	256
+#define CONFIG_SYS_LONGHELP							/* undef to save memory */
+#define CONFIG_SYS_CBSIZE			256
 /* Print Buffer Size */
-#define CONFIG_SYS_PBSIZE	(CONFIG_SYS_CBSIZE + \
-				sizeof(CONFIG_SYS_PROMPT)+16)
-#define CONFIG_SYS_MAXARGS	16
-#define CONFIG_SYS_BARGSIZE	CONFIG_SYS_CBSIZE
+#define CONFIG_SYS_PBSIZE			(CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT)+16)
+#define CONFIG_SYS_MAXARGS			16
+#define CONFIG_SYS_BARGSIZE			CONFIG_SYS_CBSIZE
 
-#define CONFIG_SYS_MEMTEST_START	0x30000000	/* memtest works on */
-#define CONFIG_SYS_MEMTEST_END		0x33F00000	/* 63 MB in DRAM */
+#define CONFIG_SYS_MEMTEST_START	0x30000000		/* memtest works on */
+#define CONFIG_SYS_MEMTEST_END		0x33F00000		/* 63 MB in DRAM */
 
 #define CONFIG_SYS_LOAD_ADDR		0x30800000
 
@@ -122,11 +128,11 @@
 /*-----------------------------------------------------------------------
  * Physical Memory Map
  */
-#define CONFIG_NR_DRAM_BANKS	1          /* we have 1 bank of DRAM */
-#define PHYS_SDRAM_1		0x30000000 /* SDRAM Bank #1 */
-#define PHYS_SDRAM_1_SIZE	0x04000000 /* 64 MB */
+#define CONFIG_NR_DRAM_BANKS		1				/* we have 1 bank of DRAM */
+#define PHYS_SDRAM_1				0x30000000		/* SDRAM Bank #1 */
+#define PHYS_SDRAM_1_SIZE			0x04000000		/* 64 MB */
 
-#define PHYS_FLASH_1		0x00000000 /* Flash Bank #0 */
+#define PHYS_FLASH_1				0x00000000		/* Flash Bank #0 */
 
 #define CONFIG_SYS_FLASH_BASE	PHYS_FLASH_1
 
@@ -143,15 +149,15 @@
 #define CONFIG_FLASH_CFI_DRIVER
 #define CONFIG_FLASH_CFI_LEGACY
 #define CONFIG_SYS_FLASH_LEGACY_512Kx16
-#define CONFIG_FLASH_SHOW_PROGRESS	45
+#define CONFIG_FLASH_SHOW_PROGRESS			45
 
-#define CONFIG_SYS_MAX_FLASH_BANKS	1
-#define CONFIG_SYS_FLASH_BANKS_LIST     { CONFIG_SYS_FLASH_BASE }
-#define CONFIG_SYS_MAX_FLASH_SECT	(35)		/* MX29LV160DB */
+#define CONFIG_SYS_MAX_FLASH_BANKS			1
+#define CONFIG_SYS_FLASH_BANKS_LIST			{ CONFIG_SYS_FLASH_BASE }
+#define CONFIG_SYS_MAX_FLASH_SECT			(35)						/* MX29LV160DB */
 
-#define CONFIG_ENV_ADDR			(CONFIG_SYS_FLASH_BASE + 0x070000)
+#define CONFIG_ENV_ADDR						(CONFIG_SYS_FLASH_BASE + 0x070000)
 #define CONFIG_ENV_IS_IN_FLASH
-#define CONFIG_ENV_SIZE			0x10000
+#define CONFIG_ENV_SIZE						0x10000
 /* allow to overwrite serial and ethaddr */
 #define CONFIG_ENV_OVERWRITE
 
@@ -159,10 +165,10 @@
  * Size of malloc() pool
  * BZIP2 / LZO / LZMA need a lot of RAM
  */
-#define CONFIG_SYS_MALLOC_LEN	(4 * 1024 * 1024)
+#define CONFIG_SYS_MALLOC_LEN				(4 * 1024 * 1024)
 
-#define CONFIG_SYS_MONITOR_LEN	(448 * 1024)
-#define CONFIG_SYS_MONITOR_BASE	CONFIG_SYS_FLASH_BASE
+#define CONFIG_SYS_MONITOR_LEN				(448 * 1024)
+#define CONFIG_SYS_MONITOR_BASE				CONFIG_SYS_FLASH_BASE
 
 /*
  * NAND configuration
@@ -170,24 +176,26 @@
 #ifdef CONFIG_CMD_NAND
 #define CONFIG_NAND_S3C2440
 #define CONFIG_SYS_S3C2440_NAND_HWECC
-#define CONFIG_SYS_MAX_NAND_DEVICE	1
-#define CONFIG_SYS_NAND_BASE		0x4E000000
+#define CONFIG_SYS_MAX_NAND_DEVICE			1
+#define CONFIG_SYS_NAND_BASE				0x4E000000
 #endif
 
 /*
  * File system
  */
+/* Some Macros are defined in configs/smdk2440_defconfig */
+#ifndef CONFIG_USER_CUT
 #define CONFIG_CMD_UBIFS
 #define CONFIG_CMD_MTDPARTS
 #define CONFIG_MTD_DEVICE
 #define CONFIG_MTD_PARTITIONS
 #define CONFIG_YAFFS2
 #define CONFIG_RBTREE
+#endif
 
 /* additions for new relocation code, must be added to all boards */
-#define CONFIG_SYS_SDRAM_BASE	PHYS_SDRAM_1
-#define CONFIG_SYS_INIT_SP_ADDR	(CONFIG_SYS_SDRAM_BASE + 0x1000 - \
-				GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_SDRAM_BASE				PHYS_SDRAM_1
+#define CONFIG_SYS_INIT_SP_ADDR				(CONFIG_SYS_SDRAM_BASE + 0x1000 - GENERATED_GBL_DATA_SIZE)
 
 #define CONFIG_BOARD_EARLY_INIT_F
 
